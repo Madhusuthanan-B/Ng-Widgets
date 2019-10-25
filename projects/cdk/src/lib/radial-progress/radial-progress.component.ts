83e63f0416e94f92fe599f070f06e6339b9611b4
Madhusuthanan B <madhusuthanan.b@kdi.kongsberg.com>
2019-10-26 00:16:07 +0530

    Including d3

--- projects/cdk/src/lib/radial-progress/radial-progress.component.ts
+++ projects/cdk/src/lib/radial-progress/radial-progress.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as d3 from 'd3';
 
 @Component({
   selector: 'lib-radial-progress',
@@ -10,6 +11,7 @@ export class RadialProgressComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    console.log(d3);
   }
 
 }
