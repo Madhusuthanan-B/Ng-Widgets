d1681ef9dc84f40d8951ffac068d66e0ceedc62f
Madhusuthanan B <madhusuthanan.b@kdi.kongsberg.com>
2019-10-25 23:59:22 +0530

    Including initial radial-progress module as part of cdk

--- projects/cdk/src/lib/cdk.module.ts
+++ projects/cdk/src/lib/cdk.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CdkComponent } from './cdk.component';
+import { RadialProgressModule } from './radial-progress/radial-progress.module';
 
 @NgModule({
   declarations: [CdkComponent],
   imports: [
+    RadialProgressModule
   ],
-  exports: [CdkComponent]
+  exports: [CdkComponent, RadialProgressModule]
 })
 export class CdkModule { }
---
+++ projects/cdk/src/lib/radial-progress/radial-progress.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-radial-progress',
+  templateUrl: './radial-progress.component.html',
+  styleUrls: ['./radial-progress.component.css']
+})
+export class RadialProgressComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
